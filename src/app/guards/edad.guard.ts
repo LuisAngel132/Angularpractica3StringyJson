4071034374d2a148eed085cccf2626f882b72177
Your Name <you@example.com>
2021-01-25 17:32:03 -0600

    second2 commit

--- src/app/guards/edad.guard.ts
+++ src/app/guards/edad.guard.ts
@@ -1,16 +1,18 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import{Router,ActivatedRoute} from '@angular/router'
 
 @Injectable({
   providedIn: 'root'
 })
 export class EdadGuard implements CanActivate {
+  constructor(private router:Router,private route:ActivatedRoute) { }
+
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): boolean {
-      const edad=20;
-      if(edad>18){
+      if(this.route){
     return true;
       }
 return false;
